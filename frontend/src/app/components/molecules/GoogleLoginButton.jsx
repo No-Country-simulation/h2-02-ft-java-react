d3e743a4e743412a86e4b014e368b5d904cd5e9e
mateok13 <mateoroserok13@gmail.com>
2024-10-09 19:40:38 -0500

    usando el endpoint de crear usuario

--- frontend/src/app/components/molecules/GoogleLoginButton.jsx
+++ frontend/src/app/components/molecules/GoogleLoginButton.jsx
@@ -2,11 +2,11 @@ import Button from '../atoms/Button';
 
 export default function GoogleLoginButton() {
   return (
-    <Button className="h-[45px] border border-inputBorder relative flex w-full items-center justify-center bg-inputBackground text-black rounded-full">
+    <Button className="relative flex h-[45px] w-full items-center justify-center rounded-full border border-inputBorder bg-inputBackground text-black">
       <img
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png"
         alt="Google Logo"
-        className="absolute left-4 h-8 w-8 bg-white p-1 rounded-full"
+        className="absolute left-4 h-8 w-8 rounded-full bg-white p-1"
       />
       <span className="mx-auto">Continuar con Google</span>
     </Button>
--- frontend/src/app/components/organisms/RegisterForm.jsx
+++ frontend/src/app/components/organisms/RegisterForm.jsx
@@ -1,31 +1,99 @@
+import { useState } from 'react';
+import { createUser } from '../../services/userService';
 import InputField from '../atoms/InputField';
 import PasswordInput from '../molecules/PasswordInput';
 import Button from '../atoms/Button';
 import GoogleLoginButton from '../molecules/GoogleLoginButton';
 
 export default function RegisterForm() {
-    return (
-        <form className="flex w-full max-w-md flex-col justify-center bg-white p-8 sm:rounded-lg sm:shadow-lg">
-            <h2 className="mb-2 text-xl font-bold text-blue-600">Bienvenido a waki,</h2>
-            <p className="mb-6 text-gray-500">Crea tu cuenta completando los datos</p>
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
 
-            <div className="flex flex-col gap-3">
-                <InputField label="Nombre de usuario" type="text" />
-                <InputField label="Ingresa tu email o teléfono" type="text" />
-                <PasswordInput label="Contraseña" />
-                <PasswordInput label="Repetir contraseña" />
-            </div>
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
-            <Button className="h-[35px] my-6 bg-buttonPurple text-white">Registrarse</Button>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { username, email, password, confirmPassword } = formData;
 
-            <div className="mb-4 flex items-center justify-center relative">
-                <span className="text-black-400 relative z-10 bg-white px-2">o inicia sesión con</span>
-                <div className="absolute inset-0 flex items-center">
-                    <div className="w-full border-t border-black"></div>
-                </div>
-            </div>
+    if (password !== confirmPassword) {
+      alert('Las contraseñas no coinciden');
+      return;
+    }
 
-            <GoogleLoginButton />
-        </form>
-    );
+    try {
+      const response = await createUser(username, email, password);
+      console.log('Usuario creado:', response);
+    } catch (error) {
+      console.error('Error al crear el usuario:', error);
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex w-full max-w-md flex-col justify-center bg-white p-8 sm:rounded-lg sm:shadow-lg"
+    >
+      <h2 className="mb-2 text-xl font-bold text-blue-600">
+        Bienvenido a waki,
+      </h2>
+      <p className="mb-6 text-gray-500">Crea tu cuenta completando los datos</p>
+
+      <div className="flex flex-col gap-3">
+        <InputField
+          label="Nombre de usuario"
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+        <InputField
+          label="Ingresa tu email o teléfono"
+          type="text"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <PasswordInput
+          label="Contraseña"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <PasswordInput
+          label="Repetir contraseña"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+        />
+      </div>
+
+      <Button
+        type="submit"
+        className="my-6 h-[35px] bg-buttonPurple text-white"
+      >
+        Registrarse
+      </Button>
+
+      <div className="relative mb-4 flex items-center justify-center">
+        <span className="text-black-400 relative z-10 bg-white px-2">
+          o inicia sesión con
+        </span>
+        <div className="absolute inset-0 flex items-center">
+          <div className="w-full border-t border-black"></div>
+        </div>
+      </div>
+
+      <GoogleLoginButton />
+    </form>
+  );
 }
