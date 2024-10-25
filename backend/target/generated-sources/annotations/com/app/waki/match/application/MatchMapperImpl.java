868d3af95466e76cd9fd1e8bfa3bcf0f701f759b
mateok13 <mateoroserok13@gmail.com>
2024-10-25 12:22:57 -0500

    intentando merge

--- backend/target/generated-sources/annotations/com/app/waki/match/application/MatchMapperImpl.java
+++ backend/target/generated-sources/annotations/com/app/waki/match/application/MatchMapperImpl.java
@@ -18,9 +18,9 @@ import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
 
 @Generated(
-    value = "org.mapstruct.ap.MappingProcessor"
-
-
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-10-25T12:21:52-0500",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.40.0.v20240919-1711, environment: Java 17.0.12 (Eclipse Adoptium)"
 )
 @Component
 public class MatchMapperImpl implements MatchMapper {
