7b8a51387fafecfbf030636f8e056dc8a2d05004
mateok13 <mateoroserok13@gmail.com>
2024-10-28 17:10:19 -0500

    lets go

--- frontend/src/app/components/organisms/Rewards.jsx
+++ frontend/src/app/components/organisms/Rewards.jsx
@@ -5,7 +5,7 @@ import PointsProgress from '../molecules/PointsProgress';
 import MonthlyRewards from '../molecules/MonthlyRewards';
 
 export default function Rewards({ divisionData }) {
-  const { division, points } = divisionData;
+  const { division, points, username } = divisionData;
 
   const divisions = {
     bronce: DivisionBronce,
@@ -23,20 +23,30 @@ export default function Rewards({ divisionData }) {
 
   return (
     <main className="flex w-full flex-col items-center overflow-hidden p-4 sm:min-w-[570px]">
-      <img
-        src={divisions[division]}
-        alt={`Division ${division}`}
-        className="mb-4 h-[105.33px]"
-      />
-      <div className="text-center text-[18px] text-[#555555]">Estás en la</div>
-      <div className="pb-7 text-center text-[22px] font-semibold text-blueWaki">
-        {divisionTitles[division]}
-      </div>
-      <PointsProgress
-        myPoints={points}
-        necessaryPoints={necessaryPoints}
-        currentDivision={division}
-      />
+      {division === 'LIMBO' ? (
+        <p className="text-center text-[18px] text-[#181818]">
+          Debes ganar puntos para clasificarte.
+        </p>
+      ) : (
+        <>
+          <img
+            src={divisions[division]}
+            alt={`Division ${division}`}
+            className="mb-4 h-[105.33px]"
+          />
+          <div className="text-center text-[18px] text-[#555555]">
+            Estás en la
+          </div>
+          <div className="pb-7 text-center text-[22px] font-semibold text-blueWaki">
+            {divisionTitles[division]}
+          </div>
+          <PointsProgress
+            myPoints={points}
+            necessaryPoints={necessaryPoints}
+            currentDivision={division}
+          />
+        </>
+      )}
       <MonthlyRewards divisions={divisions} divisionTitles={divisionTitles} />
     </main>
   );
--- frontend/src/app/pages/Divisions.jsx
+++ frontend/src/app/pages/Divisions.jsx
@@ -5,25 +5,26 @@ import Ranking from '../components/organisms/Ranking';
 import Rewards from '../components/organisms/Rewards';
 import Quests from '../components/organisms/Quests';
 import { useAuth } from '../context/AuthContext';
-import { getUserRanking } from '../services/matchService';
+import { getUserRanking } from '../services/divisionService';
 
 export default function Divisions() {
   const [selectedView, setSelectedView] = useState('ranking');
-  const [divisionData, setDivisionData] = useState(null);
+  const [divisionData, setDivisionData] = useState([]);
   const { userId } = useAuth();
 
   useEffect(() => {
     const fetchUserRanking = async () => {
       try {
         const data = await getUserRanking(userId);
-        console.log("ranking:" ,data)
         setDivisionData(data);
       } catch (error) {
         console.error('Error al obtener el ranking del usuario:', error);
       }
     };
 
-    fetchUserRanking();
+    if (userId) {
+      fetchUserRanking();
+    }
   }, [userId]);
 
   const renderContent = () => {
