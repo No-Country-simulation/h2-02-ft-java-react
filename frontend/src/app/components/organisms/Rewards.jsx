48954eb0cdef91d5151547c641463119ee0a48d7
mateok13 <mateoroserok13@gmail.com>
2024-10-28 06:57:44 -0500

    hice lo que pude xd

--- frontend/src/app/components/organisms/Rewards.jsx
+++ frontend/src/app/components/organisms/Rewards.jsx
@@ -2,7 +2,7 @@ import DivisionBronce from '../../../assets/bronce.png';
 import DivisionPlata from '../../../assets/plata.png';
 import DivisionOro from '../../../assets/oro.png';
 import PointsProgress from '../molecules/PointsProgress';
-import { MdKeyboardArrowRight } from 'react-icons/md';
+import MonthlyRewards from '../molecules/MonthlyRewards';
 
 export default function Rewards({ divisionData }) {
   const { division, points } = divisionData;
@@ -37,36 +37,7 @@ export default function Rewards({ divisionData }) {
         necessaryPoints={necessaryPoints}
         currentDivision={division}
       />
-      <div className="mt-4 h-[441.17px] w-full rounded-lg p-3 shadow-custom">
-        <div className="text-[22px] font-semibold text-blueWaki">
-          ¡Premios todos los meses!
-        </div>
-        {['oro', 'plata', 'bronce'].map((div) => (
-          <div
-            key={div}
-            className="mt-4 flex h-[112.14px] items-center rounded-lg bg-[#F3F4F5] p-4"
-          >
-            <img
-              src={divisions[div]}
-              alt={`Division ${div}`}
-              className="mr-4 h-[65px]"
-            />
-            <div className="flex-grow">
-              <div className="text-[16px] text-[#181818]">
-                {divisionTitles[div]}
-              </div>
-              <div className="text-[14px] text-[#555555]">
-                {div === 'bronce'
-                  ? 'Esta división no ofrece recompensas. Sube a la división plata para participar por premios.'
-                  : 'Descubre las recompensas de esta división.'}
-              </div>
-            </div>
-            {div !== 'bronce' && (
-              <MdKeyboardArrowRight className="text-blueWaki" size={24} />
-            )}
-          </div>
-        ))}
-      </div>
+      <MonthlyRewards divisions={divisions} divisionTitles={divisionTitles} />
     </main>
   );
 }
