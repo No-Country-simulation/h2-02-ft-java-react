1599248d3c021a7316fef367836a7e799c9514e3
mateok13 <mateoroserok13@gmail.com>
2024-11-14 11:45:45 -0500

    listo

--- frontend/src/app/pages/Profile.jsx
+++ frontend/src/app/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import FooterNavbar from '../components/organisms/FooterNavbar';
 import ProfileHeader from '../components/molecules/ProfileHeader';
 import ProfileList from '../components/organisms/ProfileList';
-import RankingCard from '../components/molecules/rankingCard';
+import RankingCard from '../components/molecules/RankingCard';
 import { Outlet, useLocation } from 'react-router-dom';
 
 export default function Profile() {
